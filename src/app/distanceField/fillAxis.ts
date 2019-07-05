b5fefaabb1621785a97996a4f3a5b464d6b9a3b2
mmanyshev <mikhail.manyshev@firstlinesoftware.com>
2019-07-05 06:01:57 +0300

    Parallel axis evaluation

--- src/app/distanceField/fillAxis.ts
+++ src/app/distanceField/fillAxis.ts
@@ -10,19 +10,15 @@ export function fillAxis(getDotAt: DotSelectorFn, mainAxisLen: number, crossAxis
 
     for (let mainAxisPos = 1; mainAxisPos < mainAxisLen; mainAxisPos += 1) {
 
-      const currentDot = getDotAt(mainAxisPos, crossAxisPos);
-      const previousDot = getDotAt(mainAxisPos - 1, crossAxisPos);
+      const leftCursor = getDotAt(mainAxisPos, crossAxisPos);
+      const leftPreCursor = getDotAt(mainAxisPos - 1, crossAxisPos);
 
-      currentDot.suggestMinDistanceWith(previousDot);
+      leftCursor.suggestMinDistanceWith(leftPreCursor);
 
-    }
+      const rightCursor = getDotAt(mainAxisLen - mainAxisPos - 1, crossAxisPos);
+      const rightPreCursor = getDotAt(mainAxisLen - mainAxisPos, crossAxisPos);
 
-    for (let mainAxisPos = mainAxisLen - 2; mainAxisPos > -1; mainAxisPos -= 1) {
-
-      const currentDot = getDotAt(mainAxisPos, crossAxisPos);
-      const previousDot = getDotAt(mainAxisPos + 1, crossAxisPos);
-
-      currentDot.suggestMinDistanceWith(previousDot);
+      rightCursor.suggestMinDistanceWith(rightPreCursor);
 
     }
 
